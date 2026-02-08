85da98f564655013479899e476cb988cbc993610
TharushiSandeepani <tharushif01@gmail.com>
2026-02-08 10:01:41 +0530

    LSP concept in SOLID

---
+++ Good-L-practice.java
@@ -0,0 +1,17 @@
+interface Shape {
+	void draw();
+}
+
+class Rectangle implements Shape {
+	@Override
+	public void draw() {
+		// Code to draw a rectangle
+	}
+}
+
+class Square implements Shape {
+	@Override
+	public void draw() {
+		// Code to draw a square
+	}
+}

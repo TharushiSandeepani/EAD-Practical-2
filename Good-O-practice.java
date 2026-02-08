a330a9d09b97b46c5a6423591c9b9b7c716aa6d0
TharushiSandeepani <tharushif01@gmail.com>
2026-02-08 09:46:22 +0530

    OCP concept in SOLID

---
+++ Good-O-practice.java
@@ -0,0 +1,25 @@
+
+interface Shape {
+	void draw();
+}
+
+class Rectangle implements Shape {
+	@Override
+	public void draw() {
+		// Code to draw a rectangle
+	}
+}
+
+class Circle implements Shape {
+	@Override
+	public void draw() {
+		// Code to draw a circle
+	}
+}
+
+class Triangle implements Shape {
+	@Override
+	public void draw() {
+		// Code to draw a triangle
+	}
+}
